1027690dfa697a5298b5127b08b6db240baff321
MD MAHAMADUL HASAN SHAIKAT <113624647+MHSHAIKAT95@users.noreply.github.com>
2022-12-02 00:25:10 +0600

    Java program print that year is a leap year or not using if else.

---
+++ leapYear.java
@@ -0,0 +1,31 @@
+package firstjava;
+
+import java.util.Scanner;
+
+
+
+
+public class FirstJava {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    Scanner sc = new Scanner(System.in);
+    
+        System.out.print("Enter The Year :");
+        int year = sc.nextInt();
+        
+        
+        
+        if((year%4 == 0 && year%100==0) || (year%400==0))
+        {
+
+            System.out.printf("%d Is a leap Year\n", year);
+        }else
+        {
+            System.out.printf("%d Is not a leap year\n", year);
+        }
+
+
+ }
+}
