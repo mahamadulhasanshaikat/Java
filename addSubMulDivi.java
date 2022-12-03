726fffb474c9c3429e0fb155f9248b05aaa27848
MD MAHAMADUL HASAN SHAIKAT <113624647+MHSHAIKAT95@users.noreply.github.com>
2022-12-03 13:42:39 +0600

    two integers as inputs and adds, subtraction, multiplication, and division.

---
+++ addSubMulDivi.java
@@ -0,0 +1,37 @@
+package firstjava;
+
+import java.util.Scanner;
+
+
+
+
+public class FirstJava {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    Scanner sc = new Scanner(System.in);
+    
+        System.out.print("Enter first int number : ");
+        int a = sc.nextInt();
+        
+        System.out.print("Enter secound int number : ");
+        int b = sc.nextInt();
+        
+        
+        int add = a+b;
+        System.out.printf("The sum of two number is %d\n",add);
+        
+        int sub = a-b;
+        System.out.printf("The subtraction two number is %d\n",sub);
+        
+        int mult = a*b;
+        System.out.println("The multiplication two number is " +mult);
+        
+        int divi = a/b;
+        System.out.println("The division two number is "+divi);
+        
+        
+        
+    }    
+}
