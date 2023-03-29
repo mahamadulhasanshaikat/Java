a011d0c0ae33af910f4863489a80eca01dc24b65
MD MAHAMADUL HASAN SHAIKAT <113624647+MHSHAIKAT95@users.noreply.github.com>
2023-03-30 00:25:10 +0600

    Create 1To10UsingAForLoop.java
     Write a program that prints out the numbers from 1 to 10 using a for loop.

---
+++ 1To10UsingAForLoop.java
@@ -0,0 +1,10 @@
+
+public class SimpleJavaProject {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
