d0681bb9c39317090a82971093e8b7c8a5b3335a
MD MAHAMADUL HASAN SHAIKAT <113624647+MHSHAIKAT95@users.noreply.github.com>
2023-03-30 00:26:45 +0600

    Create  largest number.java
    Write a program that prompts the user to enter three numbers and then prints out the largest number.

---
+++ largest number.java
@@ -0,0 +1,32 @@
+
+
+import java.util.Scanner;
+
+public class SimpleJavaProject {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("Enter third number: ");
+        double num3 = scanner.nextDouble();
+
+        double max = num1;
+
+        if (num2 > max) {
+            max = num2;
+        }
+
+        if (num3 > max) {
+            max = num3;
+        }
+
+        System.out.println("The largest number is " + max);
+    }
+}
+
